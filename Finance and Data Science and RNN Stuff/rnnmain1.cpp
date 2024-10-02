0e8d31647601e9c7809ff36ee6e090d0014ebd8c
Willie169 <cg10250207@gmail.com>
2024-10-02 16:46:53 +0800

    t

--- Finance and Data Science and RNN Stuff/rnnmain1.cpp
+++
@@ -1,8 +0,0 @@
-#include "rnnmain.hpp"
-
-int main() {
-    cout << "Training Model: 15 21 10 100 0.01 5" << endl;
-	int a = trainModel(15, 21, 10, 100, 0.01, 5, "trainedModel_15_21_10_100_0.01_5");
-    tuple<vector<vector<matrix>>, vector<double>> b = load(21, 23, "trainedModel_15_21_10_100_0.01_5");
-	return 0;
-}
