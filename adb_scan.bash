f578e634cfc4a74f5e3a77f6e2bfaf32168dd14b
Willie169 <cg10250207@gmail.com>
2024-10-03 13:34:20 +0800

    y

--- adb_scan.bash
+++ adb_scan.bash
@@ -1,14 +1,26 @@
-#!/bin/bash
+mkdir -p "$PREFIX/adb_scan"
+file="$PREFIX/adb_scan/adb_scan_result.txt"
+touch "$file"
 
-file="file.txt"
-> "$file"
-
-for (( i=128; i<192; i++ )); do
+for (( i=0; i<240; i++ )); do
+    if (( i==10 )); then
+        continue
+    fi
     for (( j=0; j<256; j++ )); do
+        if (( i==172 && j == 16 )); then
+            j=31
+            continue
+        fi
+        if (( i==192 && j==168 )); then
+            continue
+        fi
         for (( k=0; k<256; k++ )); do
             for (( l=0; l<256; l++ )); do
                 ip="$i.$j.$k.$l"
-                echo "testing $ip"
+                if [[ "$ip" == "127.0.0.1" ]]; then
+                    continue
+                fi
+                echo "Testing $ip"
                 timeout 15 adb connect "$ip" 2>/dev/null
                 output=$(adb devices)
                 adb disconnect >/dev/null
